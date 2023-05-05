c97e44bbf72ea59822aac2580befce70753dfac2
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-05-05 18:24:28 -0300

    implement day in monthDays

--- src/calendar/Calendar.jsx
+++ src/calendar/Calendar.jsx
@@ -1,9 +1,5 @@
 /* eslint-disable react/prop-types */
 import { Grid } from "@mui/material";
-import { Weekdays } from "./components/calendar/Weekdays";
-import { FillDays } from "./components/calendar/FillDays";
-import { HeaderCalendario } from "./components/calendar/HeaderCalendar";
-import { DaysOfTheMonth2 } from "./components/calendar/DaysOfTheMonth2";
 import { useCalendar } from "../hooks/useCalendar";
 
 export const Calendar = ({ currentDate = new Date(), onChange }) => {
--- src/components/MonthDays.jsx
+++ src/components/MonthDays.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { getDate, getMonth, getYear } from "date-fns";
+import { Day } from "./Day";
 
 const disabledDates = [
   new Date(2023, 5, 12),
@@ -12,11 +13,21 @@ export const MonthDays = ({ allDays = [] }) => {
   return (
     <>
       {allDays.map((fecha, index) => {
-        const dayNumber = getDate(fecha);
+        const day = getDate(fecha);
         const month = getMonth(fecha) + 1;
         const year = getYear(fecha);
 
-        return <></>;
+        return (
+          <>
+            <Day
+              key={index}
+              day={day}
+              month={month}
+              year={year}
+              disabledDates={disabledDates}
+            />
+          </>
+        );
       })}
     </>
   );
