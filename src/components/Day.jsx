66f75fd24002d15aeb001f0ff16dfa1a467bfedf
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-05-05 18:23:05 -0300

    add <Day/>

---
+++ src/components/Day.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+
+import { Grid } from "@mui/material";
+import { useDay } from "../hooks/useDay";
+
+export const Day = ({ day, month, year, disabledDates }) => {
+  const {
+    dateFormat,
+    isTodayDate,
+    isWeekendDate,
+    isPastDay,
+    buttonDisabled,
+    isDisabledDate,
+  } = useDay(day, month, year, disabledDates);
+
+  const handleClickDate = (fecha) => {
+    console.log(fecha);
+  };
+
+  return (
+    <>
+      <Grid
+        item
+        xs={1}
+        sm={1}
+        md={1}
+        lg={1}
+        minHeight={50}
+        height={50}
+        sx={{
+          border: "1px solid #080808",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "black",
+          backgroundColor: isTodayDate
+            ? "#7c8cf7"
+            : isPastDay
+            ? "#e0dcdc"
+            : isWeekendDate
+            ? "#f5b5bc"
+            : isDisabledDate // Si es una fecha deshabilitada, se aplica el color verde
+            ? "#b3ffb3"
+            : "#b5cef5",
+          fontWeight: isTodayDate ? "bold" : "normal",
+        }}
+        onClick={() => handleClickDate(dateFormat)}
+        component={"button"}
+        disabled={buttonDisabled}
+      >
+        {day}
+      </Grid>
+    </>
+  );
+};
