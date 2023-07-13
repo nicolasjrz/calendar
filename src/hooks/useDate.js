c3c6040ef75e70ac1f18c3fece6e358e933809da
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-07-13 19:15:01 -0300

    cambio de nombre de isWeekendDate a isClosedDay y cambio de logica

--- src/hooks/useDate.js
+++ src/hooks/useDate.js
@@ -13,18 +13,21 @@ export const useDate = (day, mes, anio, disabledDates = []) => {
   const date = setDate(setMonth(setYear(new Date(), anio), mes - 1), day);
 
   const isTodayDate = isSameDay(date, new Date());
-  const isWeekendDate = isWeekend(date);
+
+  //const isWeekendDate = date.getDay() === 0; // Deshabilita si es domingo (0 representa domingo, 1 representa lunes, etc.)
+  const isClosedDay = date.getDay() === 0 || date.getDay() === 1; // Deshabilita si es domingo o lunes
+  //const isWeekendDate = isWeekend(date);
   const isPastDay = isBefore(date, new Date());
   const isDisabledDate = disabledDates.some((disabledDate) =>
     isSameDay(date, disabledDate)
   );
   const isDiaSeleccionado = daySelected ? isSameDay(date, daySelected) : false;
-  const buttonDisabled = isWeekendDate || isPastDay || isDisabledDate;
+  const buttonDisabled = isClosedDay || isPastDay || isDisabledDate;
 
   return {
     date,
     isTodayDate,
-    isWeekendDate,
+    isClosedDay,
     isPastDay,
     buttonDisabled,
     isDisabledDate,
